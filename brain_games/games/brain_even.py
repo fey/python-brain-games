9782dd1e726a6dcf10e39802b78e5193d10d5a92
Nikolay Gagarinov <feycot@gmail.com>
2023-11-29 00:30:27 +0500

    add structure

---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,29 @@
+import random
+
+from brain_games.engine import run_game, ROUNDS_COUNT
+
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def is_even(n):
+    return n % 2 == 0
+
+
+def build_round():
+    question = random.randint(1, 100)
+    answer = 'yes' if is_even(question) else 'no'
+
+    return (question, answer)
+
+
+def start_even():
+    i = 0
+
+    rounds = []
+
+    while (i < ROUNDS_COUNT):
+        round = build_round()
+        rounds.append(round)
+        i += 1
+
+    run_game(DESCRIPTION, rounds)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,32 +1,6 @@
-import random
+#!/usr/bin/env python3
 
-from .engine import run_game, ROUNDS_COUNT
-
-DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
-
-
-def is_even(n):
-    return n % 2 == 0
-
-
-def build_round():
-    question = random.randint(1, 100)
-    answer = 'yes' if is_even(question) else 'no'
-
-    return (question, answer)
-
-
-def start_even():
-    i = 0
-
-    rounds = []
-
-    while (i < ROUNDS_COUNT):
-        round = build_round()
-        rounds.append(round)
-        i += 1
-
-    run_game(DESCRIPTION, rounds)
+from brain_games.games.brain_even import start_even
 
 
 def main():
