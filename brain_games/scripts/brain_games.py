b0825bad778165804b54f1684b9ee7effc6372c0
Nikolay Gagarinov <feycot@gmail.com>
2023-11-28 23:16:07 +0500

    add prompt

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import prompt
+
+
 def greet():
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
     print("Welcome to the Brain Games!")
 
 
